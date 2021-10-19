11f01c079867d7bec01372fa93ed386ce918bed6
luisbelisario <luisbls77@hotmail.com>
2021-10-19 15:58:35 -0300

    Finalizei o projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import biscoito from './assets/luck.png'
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            frase_escolhida: ''
+        }
+
+        this.quebrarBiscoito = this.quebrarBiscoito.bind(this)
+
+        this.frases = ['A vida trará coisas boas se tiveres paciência.',
+                       'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+                       'O bom-senso vai mais longe do que muito conhecimento.',
+                       'Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.',
+                       'Amizade e Amor são coisas que podem virar uma só num piscar de olhos.',
+                       'Faça pequenas coisas agora e maiores coisas lhe serão confiadas cada dia.',
+                       'A paciência na adversidade é o sinal de um coração sensível.',
+                       'O futuro pertence àqueles que acreditam na beleza de seus sonhos.',
+                       'O verdadeiro homem mede a sua força, quando se defronta com o obstáculo.',
+                       'A adversidade desperta em nós capacidades que, em circunstâncias favoráveis, teriam ficado adormecidas.',
+                       'Inspiração vem dos outros. Motivação vem de dentro de nós.']
+    }
+
+    quebrarBiscoito() {
+        let state = this.state
+        let num = Math.floor(Math.random() * this.frases.length)
+        state.frase_escolhida = '"' + this.frases[num] + '"'
+        this.setState(state)
+    }
+
+    render() {
+      return(
+        <div className="container">
+            <img src={biscoito} alt="" className="img"/>
+            <Botao acaoBtn={this.quebrarBiscoito}/>
+            <h3 className="textoFrase">{this.state.frase_escolhida}</h3>
+        </div>
+      )
+    }
 }
+
+class Botao extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            acao: 'Abrir biscoito'
+        }
+    }
+
+    render() {
+      return(
+        <div>
+            <button className="Btn" onClick={this.props.acaoBtn}>{this.state.acao}</button>
+        </div>
+      )
+    }
+}
+
 
 export default App;
